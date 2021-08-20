2a0a5e613079e6ccb707c13a75eeed8ed1a4893a
Andy Zhu <79018636+andyzhu23@users.noreply.github.com>
2021-08-19 17:25:05 -0700

    Add files via upload

---
+++ ajax practice/AJAX.js
@@ -0,0 +1,23 @@
+const API = "394fa54bd8078b3b1c67c22a9f94ee57"
+
+function getWeather() {
+    var zip = document.getElementById("zip").value;
+    var request = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&APPID=${API}`;
+    var promise = fetch(request);
+    promise.then((response) => {
+        var jsonPromise = response.json();
+        jsonPromise.then((jsonResponse) => {
+            try {
+                var response = jsonResponse.name + ": " + jsonResponse.main.temp + "\n";
+                document.getElementById("output").value = response + document.getElementById("output").value;
+            }
+            catch {
+                document.getElementById("output").value = "code not found\n" + document.getElementById("output").value;
+            }
+        });
+    });
+}
+
+document.getElementById("clear").addEventListener("click", () => {
+    document.getElementById("output").value = "";
+});
